f020fc843d94bd632e7c310b0931fd775e2f6422
JohnieXu <281910378@qq.com>
2022-03-12 16:31:03 +0800

    fix: 窗口宽度100%

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,10 +1,4 @@
-import { printLine } from './modules/print';
 import { open as openPopup, close as closePopup } from './modules/popup';
-
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
 
 function addButton() {
   let opened = false;
