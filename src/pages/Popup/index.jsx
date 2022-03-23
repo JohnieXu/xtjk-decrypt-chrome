0569c096f4379c058ea244a83a8406f48b875393
JohnieXu <281910378@qq.com>
2022-03-23 19:56:29 +0800

    feat: react-vant 桌面端事件适配

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 import Popup from './Popup';
 import './index.css';
 
+import '@vant/touch-emulator';
+
 render(<Popup />, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
