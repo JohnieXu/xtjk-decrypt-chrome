499d6b4842f911f0bb2f232acecff806e4e81116
JohnieXu <281910378@qq.com>
2022-03-30 23:20:18 +0800

    feat: 秘钥管理页面更新&增加刷新秘钥列表,删除秘钥功能

--- src/pages/Popup/KeyManage.tsx
+++ src/pages/Popup/KeyManage.tsx
@@ -1,18 +1,21 @@
-import { Plus } from "@react-vant/icons";
+import { Plus, Replay, DeleteO } from "@react-vant/icons";
 import React, { useState, useEffect, useRef } from "react";
-import { Button, Field, Form, Popup, Toast, hooks, Cell, List, FieldInstance } from 'react-vant';
-import { saveAppKey, getAppKey, KeyItem } from "./common/storage";
+import { Button, Field, Form, Popup, Toast, hooks, Cell, List, FieldInstance, Loading, Flex, Dialog } from 'react-vant';
+import { saveAppKey, getAppKey, removeAppKey, KeyItem } from "./common/storage";
 
-import './KeyManage.scss'
+import './KeyManage.scss';
 
 type FormValue = {
   name: string
   value: string
 }
 
+const noop = () => {};
+
 const KeyManage = () => {
 
   const [popupVisible, setPopupVisible] = useState<boolean>(false);
+  const [listLoading, setListLoding] = useState<boolean>(false);
   const [form] = Form.useForm();
   const [keyList, setKeyList] = useState<KeyItem[]>([]);
 
@@ -25,8 +28,31 @@ const KeyManage = () => {
     }, 300);
   }
 
+  const handleRefreshClick = () => {
+    loadKeyList();
+  }
+
   const handleCancelClick = () => {
     setPopupVisible(false);
+  }
+
+  const handleRemoveClick = (keyItem: KeyItem) => {
+    console.log(keyItem);
+    Dialog.confirm({
+      title: '提示',
+      message: `将要删除秘钥：${keyItem}`,
+      showCancelButton: true
+    }).then(() => {
+      removeAppKey(keyItem).then(() => {
+        Toast('删除成功！');
+        loadKeyList().catch(() => {
+          Toast('刷新列表失败！');
+        });
+      }).catch((e) => {
+        console.log(e);
+        Toast('删除失败！');
+      });
+    }).catch(noop);
   }
 
   const handleFormSubmit = (data: FormValue) => {
@@ -34,25 +60,41 @@ const KeyManage = () => {
       loadKeyList();
       setPopupVisible(false);
       Toast('保存成功！');
+      data.name = '';
+      data.value = '';
     }).catch((e) => {
       Toast('保存失败！\n' + e.message);
     });
   }
 
   const loadKeyList = (): Promise<boolean> => {
+    setListLoding(true);
     return new Promise((resolve, reject) => {
-      getAppKey().then((keyList) => {
-        setKeyList(keyList);
-        resolve(true);
-      }).catch((e) => {
-        reject(e);
-      });
+      setTimeout(() => {
+        getAppKey().then((keyList) => {
+          setKeyList(keyList);
+          setListLoding(false);
+          resolve(true);
+        }).catch((e) => {
+          setListLoding(false);
+          reject(e);
+        });
+      }, 600);
     });
   };
 
   const renderKeyItem = (keyItem: KeyItem, key: number | string) => {
     return (
-      <Cell key={key}>{keyItem}</Cell>
+      <Cell
+        key={key}
+        extra={
+        <DeleteO
+          onClick={
+            () => { handleRemoveClick(keyItem); }
+          }
+          style={{ cursor: 'pointer' }}
+          />
+        }>{keyItem}</Cell>
     )
   }
 
@@ -60,18 +102,37 @@ const KeyManage = () => {
   
   return (
     <div className="keyManage">
-      <Button
-        type="primary"
-        size="small"
-        round
-        onClick={handleAddClick}
-        icon={<Plus/>}
-      >添加</Button>
-      <List finished={true}>
+      <div className="section-operation">
+        <Button.Group onClick={noop} block round>
+          <Button
+            type="primary"
+            size="small"
+            onClick={handleAddClick}
+            icon={<Plus/>}
+          >添加</Button>
+          <Button
+            type="primary"
+            size="small"
+            onClick={handleRefreshClick}
+            icon={<Replay />}
+          >刷新</Button>
+        </Button.Group>
+      </div>
       {
-        keyList.map((key, i) => renderKeyItem(key, i))
+        listLoading ? (
+          <Flex justify="center" align="center" style={{ marginTop: '200px' }}>
+            <Flex.Item flex="0 0 30px">
+              <Loading type="spinner" />
+            </Flex.Item>
+          </Flex>
+        ) : (
+          <List className="section-list" finished={true} loading={listLoading}>
+          {
+            keyList.map((key, i) => renderKeyItem(key, i))
+          }
+          </List>
+        )
       }
-      </List>
       <Popup
         visible={popupVisible}
         style={{
--- src/pages/Popup/common/storage.ts
+++ src/pages/Popup/common/storage.ts
@@ -27,6 +27,33 @@ export function saveAppKey(appkey: KeyItem): Promise<boolean> {
   })
 }
 
+export function removeAppKey(appkey: KeyItem): Promise<boolean> {
+  const APPKEY_KEY = CONST_APPKEY_KEY
+  return new Promise((resolve, reject) => {
+    if (appkey) {
+      chrome.storage.sync.get({
+        [APPKEY_KEY]: []
+      }, (result) => {
+        console.log(result)
+        let keys = result[APPKEY_KEY]
+        if (!keys.includes(appkey)) {
+          resolve(true)
+        } else {
+          keys = keys.filter((i: string) => i !== appkey)
+        }
+        console.log('[debug] keys', keys)
+        chrome.storage.sync.set({
+          [APPKEY_KEY]: keys
+        }, () => {
+          resolve(true)
+        })
+      })
+    } else {
+      resolve(true)
+    }
+  })
+}
+
 export function getAppKey(): Promise<KeyItem[]> {
   const APPKEY_KEY = CONST_APPKEY_KEY
   return new Promise((resolve, reject) => {
